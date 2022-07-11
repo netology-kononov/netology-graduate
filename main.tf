9aaa3a4bca9b641c657e677258c39fc118c98e2b
netology-kononov <hiintmy@gmail.com>
2022-07-11 19:18:12 +0300

    Step 2.4-v0.4

--- main.tf
+++ main.tf
@@ -11,13 +11,13 @@ resource "yandex_vpc_network" "vpc_net" {
 resource "yandex_vpc_subnet" "vpc_subnet-a" {
   name           = "zone-a-subnet"
   zone           = "ru-central1-a"
-  network_id     = "${yandex_vpc_network.default.id}"
+  network_id     = "${yandex_vpc_network.vpc_net.id}"
   v4_cidr_blocks = ["192.168.1.0/24"]
 }
 
 resource "yandex_vpc_subnet" "vpc_subnet-b" {
   name           = "zone-b-subnet"
   zone           = "ru-central1-b"
-  network_id     = "${yandex_vpc_network.default.id}"
+  network_id     = "${yandex_vpc_network.vpc_net.id}"
   v4_cidr_blocks = ["192.168.2.0/24"]
 }
